9f68d8b93f25e556a1abe571b5d510769c45274d
Gergana Toshkova <geritoshkova85@gmail.com>
2023-01-06 20:18:42 +0200

    feat:Add Networking API model classes and initialization

---
+++ app/src/main/java/com/example/android/postscomments/networking/DataModels.kt
@@ -0,0 +1,19 @@
+package com.example.android.postscomments.networking
+
+import com.squareup.moshi.Json
+
+// Classes ,used by Moshi, to model the network data into Kotlin objects
+// I realize that I could've used Scalars converter but I really like the possibility
+// to be able to define custom variable names,while still matching the original JSON fields' names
+
+data class Post(
+    val userId:Int,
+    @Json(name = "id") val postId:Int,
+    val title:String,
+    val body:String
+)
+
+data class Comment(
+    @Json(name = "email") val userEmail:String,
+    val body:String
+)
