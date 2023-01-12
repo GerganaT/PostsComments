c9b7505ee479edd9bda6fad6b6789a1e845c3e11
Gergana Toshkova <geritoshkova85@gmail.com>
2023-01-12 21:19:09 +0200

    feat:Add color background logic for the posts list as per project requirement

--- app/src/main/java/com/example/android/postscomments/ui/PostsCommentsViewModel.kt
+++ app/src/main/java/com/example/android/postscomments/ui/PostsCommentsViewModel.kt
@@ -1,35 +1,42 @@
 package com.example.android.postscomments.ui
 
-import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.android.postscomments.PostsCommentsRepository
 import com.example.android.postscomments.networking.Post
 import com.example.android.postscomments.networking.PostsCommentsApiServiceEntryPoint
-import kotlinx.coroutines.flow.*
+import com.example.android.postscomments.ui.ui.theme.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
-class PostsCommentsViewModel:ViewModel() {
-    private var mutablePostsList= MutableStateFlow<SnapshotStateList<Post>>(mutableStateListOf())
+class PostsCommentsViewModel : ViewModel() {
+    private var mutablePostsList = MutableStateFlow<SnapshotStateList<Post>>(mutableStateListOf())
 
-    val postsList:StateFlow<SnapshotStateList<Post>>
-    get() = mutablePostsList
+    val postsList: StateFlow<SnapshotStateList<Post>>
+        get() = mutablePostsList
 
 
     private val repository = PostsCommentsRepository(PostsCommentsApiServiceEntryPoint.apiService)
 
 
- init {
-     viewModelScope.launch {
-         mutablePostsList.value = repository.getData().toMutableStateList()
-     }
- }
+    init {
+        viewModelScope.launch {
+            mutablePostsList.value = repository.getData().toMutableStateList()
+        }
+    }
 
-
+    fun getColorPerAuthor(authorId: Int = 0): Color {
+        val authorsAndColors: HashMap<Int, Color> = hashMapOf(
+            1 to Peach200, 2 to Green200, 3 to Lime200, 4 to Pink200, 5 to Blue200,
+            6 to Teal200, 7 to Fuchsia200, 8 to Purple500, 9 to Orange200, 10 to Tomato200
+        )
+        return authorsAndColors[authorId]!!
+    }
 
 }
 //TODO fix dependency tight coupling here
